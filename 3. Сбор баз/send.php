d829e19444e9df5853c49ec69c0cd515470494bf
vologin2v <43934520+vologin2v@users.noreply.github.com>
2021-03-01 08:54:18 +0700

    3. Сбор баз

---
+++ 3. Сбор баз/send.php
@@ -0,0 +1,55 @@
+<?php
+// Файлы phpmailer
+require 'PHPMailer.php';
+require 'SMTP.php';
+require 'Exception.php';
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+$email = $_POST['mail'];
+
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $msg = "ok";
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";                                          
+    $mail->SMTPAuth   = true;
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера GMAIL
+    $mail->Username   = 'webcrewmail'; // Логин на почте
+    $mail->Password   = 'bmxreason19'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('webcrewmail@yandex.ru', 'SBORBAZ'); // Адрес самой почты и имя отправителя
+    // Получатель письм 
+    $mail->addAddress('bazsbor@yandex.ru');
+    // Прикрипление файлов к письму
+/*if (!empty($_FILES['myfile']['name'][0])) {
+    for ($ct = 0; $ct < count($_FILES['myfile']['tmp_name']); $ct++) {
+        $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['myfile']['name'][$ct]));
+        $filename = $_FILES['myfile']['name'][$ct];
+        if (move_uploaded_file($_FILES['myfile']['tmp_name'][$ct], $uploadfile)) {
+            $mail->addAttachment($uploadfile, $filename);
+        } else {
+            $msg .= 'Неудалось прикрепить файл ' . $uploadfile;
+        }
+    }   
+}*/
+        // -----------------------
+        // Само письмо
+        // -----------------------
+        $mail->isHTML(true);
+    
+        $mail->Subject = 'Новая заявка!';
+        $mail->Body    = "<b>Имя:</b> $name <br>
+        <b>Номер телефона:</b> $tel<br>
+        <b>E-mail</b> $email";
+// Проверяем отравленность сообщения
+if ($mail->send()) {
+    echo "$msg";
+} else {
+echo "Сообщение не было отправлено. Неверно указаны настройки вашей почты";
+}
+} catch (Exception $e) {
+    echo "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
